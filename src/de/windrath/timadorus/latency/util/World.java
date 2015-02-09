b1eaad26faf028afa4cfacee5f8e0bdef1ea0c1f
SWindrath <steffen.windrath@haw-hamburg.de>
2015-02-09 12:08:24 +0100

    added File that was missing in the last commit

---
+++ src/de/windrath/timadorus/latency/util/World.java
@@ -0,0 +1,31 @@
+package de.windrath.timadorus.latency.util;
+
+import de.windrath.timadorus.latency.util.network.Message;
+
+public class World {
+
+	public boolean check(Message msg) {
+		return true;
+	}
+
+	public void incorporate(Message msg) {
+		
+	}
+
+	public Message delta() {
+		return null;
+	}
+
+	public Message toMessage() {
+		return null;
+	}
+
+	public void setTimeTo(long timestamp) {
+		
+	}
+
+	public void revertTime() {
+		
+	}
+
+}
